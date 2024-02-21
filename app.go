1bd79da685b6ce3fa6950cec8e72f866d64a8fc3
MTplusWebSystem <mtpluswebsystem@gmail.com>
2024-02-21 16:51:24 -0400

    Iniciando a criação

---
+++ app.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"fmt"
+	"github.com/MTplusWebSystem/GoBotKit/botkit"
+)
+
+func main() {
+	var token string
+	var id int
+
+	fmt.Println("Token:")
+	fmt.Scan(&token)
+
+	fmt.Println("ID:")
+	fmt.Scan(&id)
+
+	bot := botkit.BotInit{
+		Token: token,
+	}
+
+
+	for {
+		if bot.ReceiveData(){
+				go func() {
+					bot.Handler("callback_query", func(event string) {
+						if bot.ChatID == id {
+							if event == "!GenTeste"{
+								bot.ForceReplyToMessage(bot.QueryMessageID,"Quantas horas:")
+							}
+						}else{
+							bot.SendMessages("Não tem permição para Utilizar esse bot!")
+						}
+
+					})
+				}()
+				go func() { 
+					bot.Handler("commands",func(event string) {
+						if bot.ChatID == id {
+							if event == "/start"{
+
+								bot.SendMessages(`
+				--------------------------------
+				SEJA BEM VINDO(a) AO BOT DragonCore
+				--------------------------------
+				para acessar o basta digitar /menu 
+								`)
+							} else if event == "/menu"{
+								layout := map[string]interface{}{
+									"inline_keyboard": [][]map[string]interface{}{
+										{
+											{"text": "Criar Usúario", "callback_data": "!suporte"},
+											{"text": "Gerar Teste", "callback_data": "!GenTeste"},
+										},
+										{
+											{"text": "Alterar Senha", "callback_data": "!suporte"},
+											{"text": "Alterar Limite", "callback_data": "!painel"},
+										},
+										{
+											{"text": "Alterar Data", "callback_data": "!suporte"},
+											{"text": "Remover", "callback_data": "!painel"},
+										},
+									},
+								}
+				
+								bot.SendButton(`
+				--------------------------------
+				Menu de gerenciamento DragonCore
+				--------------------------------
+								`, layout)
+							}
+						}else{
+							bot.SendMessages("Não tem permição para Utilizar esse bot!")
+						}
+						
+					})
+				}()
+				go func() {
+					bot.Handler("messages",func(event string) {
+						if bot.ChatID == id {
+							if bot.ReplyMessageText == "Quantas horas:"{
+								bot.SendMessages(fmt.Sprintf("Teste de %s horas",bot.Text))
+							}
+						}else{
+							bot.SendMessages("Não tem permição para Utilizar esse bot!")
+						}
+					})
+				}()
+			 
+		}
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/MTplusWebSystem/DragonBot
+
+go 1.22.0
+
+require github.com/MTplusWebSystem/GoBotKit v0.1.1
