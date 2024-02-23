035383117c1ce1dad4876789ee91c8c206f8e71a
MTplusWebSystem <mtpluswebsystem@gmail.com>
2024-02-23 07:47:27 -0400

    0.2.9

--- app.go
+++ app.go
@@ -36,43 +36,30 @@ func main() {
 		Token: token,
 	}
 	user := make([]string, 0)
-	newdate := make([]string, 0)
+	//newdate := make([]string, 0)
 	for {
 		if bot.ReceiveData(){
 				go func() {
 					bot.Handler("callback_query", func(event string) {
 						if bot.ChatID == id {
-							if event == "!GenTeste"{
-								bot.ForceReply("Quantas horas:")
-							}else if event == "!CreateUser"{
-								bot.ForceReply("Usuário:")
-							}else if event == "!relatorio"{
-								cmd := exec.Command("php", "/opt/DragonCore/menu.php", "relatoriouser")
+							switch event {
+								case "!GenTeste":
+									bot.ForceReply("Quantas horas:")
+								case "!CreateUser":
+									bot.ForceReply("Usuário:")
+								case "!relatorio":
+									cmd := exec.Command("php", "/opt/DragonCore/menu.php", "relatoriouser")
 
-								output, err := cmd.Output()
-								if err != nil {
-									fmt.Println("Erro ao executar o comando:", err)
-									return
-								}
-								outputStr := string(output)
-								bot.SendMessages(outputStr)
-							}else if event == "!deletar"{
-								bot.ForceReply("Nome do usuário:")
-							} else if event == "!AlterarDT"{
-								bot.ForceReply("Nome do usuário")
-								if bot.ReplyMessageText == "Nome do usuário"{
-									newdate = append(newdate, bot.Text)
-								    bot.ForceReply("Nova data:")
-									if bot.ReplyMessageText == "Nova data:"{
-										newdate = append(newdate, bot.Text)
-										cmd := exec.Command("php", "/opt/DragonCore/menu.php", "alterardata", newdate[1], newdate[0])
-										cmd.Run()
-										bot.SendMessages("Data alterada com sucesso")
+									output, err := cmd.Output()
+									if err != nil {
+										fmt.Println("Erro ao executar o comando:", err)
+										return
 									}
-								}
+									outputStr := string(output)
+									bot.SendMessages(outputStr)
+								case "!Deletar":
+									bot.ForceReply("Nome do usuário:")
 							}
-								
-                               
 						}else{
 							bot.SendMessages("Não tem permição para Utilizar esse bot!")
 						}
@@ -82,40 +69,40 @@ func main() {
 				go func() { 
 					bot.Handler("commands",func(event string) {
 						if bot.ChatID == id {
-							if event == "/start"{
-
-								bot.SendMessages(`
+							switch event {
+								case "/start":
+									bot.SendMessages(`
 				--------------------------------
 				SEJA BEM VINDO(a) AO BOT DragonCore
 				--------------------------------
 				para acessar o basta digitar /menu 
 								`)
-							} else if event == "/menu"{
-								layout := map[string]interface{}{
-									"inline_keyboard": [][]map[string]interface{}{
-										{
-											{"text": "Criar Usúario", "callback_data": "!CreateUser"},
-											{"text": "Gerar Teste", "callback_data": "!GenTeste"},
+								case "/menu":
+									layout := map[string]interface{}{
+										"inline_keyboard": [][]map[string]interface{}{
+											{
+												{"text": "Criar Usúario", "callback_data": "!CreateUser"},
+												{"text": "Gerar Teste", "callback_data": "!GenTeste"},
+											},
+											{
+												{"text": "Alterar Senha", "callback_data": "!AlterarSH"},
+												{"text": "Alterar Limite", "callback_data": "!AlterarLM"},
+											},
+											{
+												{"text": "Alterar Data", "callback_data": "!AlterarDT"},
+												{"text": "Remover", "callback_data": "!deleterar"},
+											},
+											{
+												{"text": "Relatório", "callback_data": "!relatorio"},
+											},
 										},
-										{
-											{"text": "Alterar Senha", "callback_data": "!AlterarSH"},
-											{"text": "Alterar Limite", "callback_data": "!AlterarLM"},
-										},
-										{
-											{"text": "Alterar Data", "callback_data": "!AlterarDT"},
-											{"text": "Remover", "callback_data": "!deleterar"},
-										},
-										{
-											{"text": "Relatório", "callback_data": "!relatorio"},
-										},
-									},
-								}
-				
-								bot.SendButton(`
-				--------------------------------
-				Menu de gerenciamento DragonCore
-				--------------------------------
-								`, layout)
+									}
+					
+									bot.SendButton(`
+					--------------------------------
+					Menu de gerenciamento DragonCore
+					--------------------------------
+									`, layout)
 							}
 						}else{
 							bot.SendMessages("Não tem permição para Utilizar esse bot!")
@@ -126,42 +113,43 @@ func main() {
 				go func() {
 					bot.Handler("messages",func(event string) {
 						if bot.ChatID == id {
-							
-							if bot.ReplyMessageText == "Quantas horas:"{
-								horas, err := strconv.Atoi(bot.Text)
-								if err != nil {
-									fmt.Println("Erro ao converter horas:", err)
-									return
-								}
-								calc := 60 * horas
-								cmd := exec.Command("php", "/opt/DragonCore/menu.php", "gerarteste", strconv.Itoa(calc))
+							switch bot.ReplyMessageText {
+								case "Usuário:":
+									user = append(user, bot.Text)
+									bot.ForceReply("Senha:")
+                                case "Gerar Teste":
+                                    bot.ForceReply("Quantas horas:")
+									horas, err := strconv.Atoi(bot.Text)
+									if err != nil {
+										fmt.Println("Erro ao converter horas:", err)
+										return
+									}
+									calc := 60 * horas
+									cmd := exec.Command("php", "/opt/DragonCore/menu.php", "gerarteste", strconv.Itoa(calc))
 
-								output, err := cmd.Output()
-								if err != nil {
-									fmt.Println("Erro ao executar o comando:", err)
-									return
-								}
-								outputStr := string(output)
-								bot.SendMessages(outputStr)
-							}else if bot.ReplyMessageText == "Usuário:"{
-								user = append(user, bot.Text)
-								bot.ForceReply("Senha:")
-							}else if bot.ReplyMessageText == "Senha:"{
-								user = append(user, bot.Text)
-                                bot.ForceReply("Limite:")
-							}else if bot.ReplyMessageText == "Limite:"{
-								user = append(user, bot.Text)
-                                bot.ForceReply("Data em dias:")
-							}else if bot.ReplyMessageText == "Data em dias:"{
-								user = append(user, bot.Text)
-								cmd := exec.Command("php", "/opt/DragonCore/menu.php", "criaruser", user[3], user[0], user[1] , user[2] )
-								cmd.Run()
-								bot.SendMessages("Usuario criados com sucesso")
-							}else if bot.ReplyMessageText == "Nome do Usuário:"{
-								cmd := exec.Command("php", "/opt/DragonCore/menu.php", "deluser", bot.Text)
-								cmd.Run()
-                                bot.SendMessages("Usuário deletado com sucesso")
-							}
+									output, err := cmd.Output()
+									if err != nil {
+										fmt.Println("Erro ao executar o comando:", err)
+										return
+									}
+									outputStr := string(output)
+									bot.SendMessages(outputStr)
+                                case "Senha:":
+									user = append(user, bot.Text)
+									bot.ForceReply("Limite:")
+                                case "Limite:":
+									user = append(user, bot.Text)
+									bot.ForceReply("Data em dias:")
+                                case "Data em dias:":
+									user = append(user, bot.Text)
+									cmd := exec.Command("php", "/opt/DragonCore/menu.php", "criaruser", user[3], user[0], user[1] , user[2] )
+									cmd.Run()
+									bot.SendMessages("Usuario criados com sucesso")
+                                case "Nome do Usuário:":
+									cmd := exec.Command("php", "/opt/DragonCore/menu.php", "deluser", bot.Text)
+									cmd.Run()
+									bot.SendMessages("Usuário deletado com sucesso")
+                            }
 						} else {
 							bot.SendMessages("Não tem permissão para utilizar esse bot!")
 						}
